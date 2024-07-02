0d467ff1a27419185d3ee30c9d0d0e498cffc143
AresAB <ares.kittell@gmail.com>
2024-07-02 14:55:57 -0700

    made a copytree function, no unit tests but it should either work in every instance or not at all, no real edge cases to worry about I thinl

---
+++ src/copytree.py
@@ -0,0 +1,15 @@
+import os
+import shutil
+
+def cp_dir(origin_path, new_path):
+	if os.path.exists(origin_path) == False: raise Exception(f"path \'{origin_path}\' is either not an existing path or requires higher permissions")
+	if os.path.exists(new_path) == False: os.mkdir(new_path)
+	contents = os.listdir(origin_path)
+	for item in contents:
+		item_path = f"{origin_path}/{item}"
+		if os.path.isfile(item_path):
+			shutil.copy(item_path, new_path)
+		else:
+			subdir_path = f"{new_path}/{item}"
+			os.mkdir(subdir_path)
+			cp_dir(item_path, subdir_path)
--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
-from textnode import TextNode
-from htmlnode import HTMLNode, LeafNode, ParentNode
+import os
+import shutil
+from copytree import cp_dir
+
+o_path = os.path.expanduser("~/workspace/github.com/AresAB/staticSiteGenerator/src")
+n_path = os.path.expanduser("~/workspace/github.com/AresAB/staticSiteGenerator/new_deleteme")
 
 def main():
-	tnode = TextNode("This is a text node", "bold")
-	print(tnode)
+	if os.path.exists(n_path) and os.path.exists(o_path): shutil.rmtree()
+	cp_dir(o_path, n_path)
 
 main()
