5eb8a69d144dc38fd2723b9dd840ff5bfe71b2f4
AresAB <ares.kittell@gmail.com>
2024-07-05 18:49:04 -0700

    updated copytree to be compatible with other file path formatting

--- src/copytree.py
+++ src/copytree.py
@@ -6,10 +6,10 @@ def cp_dir(origin_path, new_path):
 	if os.path.exists(new_path) == False: os.mkdir(new_path)
 	contents = os.listdir(origin_path)
 	for item in contents:
-		item_path = f"{origin_path}/{item}"
+		item_path = os.path.join(origin_path, item)
 		if os.path.isfile(item_path):
 			shutil.copy(item_path, new_path)
 		else:
-			subdir_path = f"{new_path}/{item}"
+			subdir_path = os.path.join(new_path, item)
 			os.mkdir(subdir_path)
 			cp_dir(item_path, subdir_path)
