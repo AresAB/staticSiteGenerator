683b4318b5897dd824c86fbdf380cda9f967c9c6
AresAB <ares.kittell@gmail.com>
2024-07-05 18:46:47 -0700

    updated content and make generate_pages_recursive, marking the end of the guided section of this project

--- src/main.py
+++ src/main.py
@@ -1,17 +1,17 @@
 import os
 import shutil
 from copytree import cp_dir
-from markdown_to_html import generate_page
+from markdown_to_html import generate_page_recursive
 
 cp_origin_path = "./static"
 cp_new_path = "./public"
-gp_origin_path = "./content/index.md"
+gp_origin_path = "./content"
 gp_template_path = "./template.html"
-gp_new_path = "./public/index.html"
+gp_new_path = "./public"
 
 def main():
 	if os.path.exists(cp_new_path) and os.path.exists(cp_origin_path): shutil.rmtree(cp_new_path)
 	cp_dir(cp_origin_path, cp_new_path)
-	generate_page(gp_origin_path, gp_template_path, gp_new_path)
+	generate_page_recursive(gp_origin_path, gp_template_path, gp_new_path)
 
 main()
--- src/markdown_to_html.py
+++ src/markdown_to_html.py
@@ -21,3 +21,19 @@ def generate_page(from_path, template_path, dest_path):
     if os.path.isdir(dest_path_dir) == False: os.makedirs(dest_path_dir)
     with open(dest_path, "w") as f: f.write(html_page)
     print(f"All complete, file '{dest_path}' created")
+
+def generate_page_recursive(from_dir_path, template_path, dest_dir_path):
+    if os.path.exists(from_dir_path) == False: raise Exception(f"path \'{from_dir_path}\' is either not an existing path or requires higher permissions")
+    if os.path.exists(dest_dir_path) == False: os.mkdir(dest_dir_path)
+    from_paths = os.listdir(from_dir_path)
+    for item in from_paths:
+        from_item_path = os.path.join(from_dir_path, item)
+        if os.path.isfile(from_item_path):
+            dest_item = item.split(".")
+            dest_item[-1] = "html"
+            dest_item = ".".join(dest_item)
+            dest_item_path = os.path.join(dest_dir_path, dest_item)
+            generate_page(from_item_path, template_path, dest_item_path)
+        else:
+            dest_item_path = os.path.join(dest_dir_path, item)
+            generate_page_recursive(from_item_path, template_path, dest_item_path)
